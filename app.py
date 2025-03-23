8b2b5e739ece997b4f8be8ead86354d6da4ee8c4
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2025-03-23 21:31:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from pages import (
 )
 
 def add_sidebar_logo():
-    st.sidebar.image("https://www.axiom-am.com/images/logo-home.svg", width=200)
+    st.sidebar.image("https://www.axiom-am.com/images/logo-home.svg", width=250)
     st.sidebar.title("AXIOM Asset Management")
 
 def main():
