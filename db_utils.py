c311d5d9c02a50e9da31046b2b29b783654bd61b
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2025-11-20 18:51:31 +0100

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -100,7 +100,7 @@ def fetch_stocks():
 
     override_real_name = "ARADEI CAPITAL"
     custom_name = "VICENNE"
-    custom_price = 470
+    custom_price = 446.6
 
     mask = df["valeur"] == override_real_name
     if mask.any():
